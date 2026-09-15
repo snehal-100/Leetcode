ce0c5180b308b46342521594a36d488f6a009c3e
Snehal Baranwal <bsnehal354@gmail.com>
2026-09-15 14:30:47 +0530

    Time: 4 ms (36.27%) | Memory: 43.7 MB (9.78%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character>st=new Stack<>();
+        int n=s.length();
+        for(int i=0;i<n;i++){
+            if(s.charAt(i)=='('||s.charAt(i)=='['||s.charAt(i)=='{'){
+                st.push(s.charAt(i));
+            }
+            else{
+                if(st.isEmpty()){
+                    return false;
+                }
+                char ch=st.pop();
+                if((s.charAt(i)==')'&&ch=='(')||(s.charAt(i)==']'&&ch=='[')||(s.charAt(i)=='}'&&ch=='{')){
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        return st.isEmpty();
+    }
+}
